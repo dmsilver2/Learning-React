753de20b0e2b6e5e9059236265442ff89ea62e59
Daniel Silver <dmsilver2@gmail.com>
2016-02-03 22:41:37 -0500

    added proxy server with hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,19 @@ var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var webpack = require('webpack');
 
-module.exports = {
+var config = {
   devtool: 'eval',
   entry: [
+    // For hot style updates
+    'webpack/hot/dev-server',
+    // The script refreshing the browser on none hot updates
+    'webpack-dev-server/client?http://localhost:8080',
     './app/main.js'
   ],
   output: {
     path: path.resolve(__dirname, "public/assets"),
     publicPath: "/assets/",
     filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: "./public",
-    inline: true,
-    port: 8080
   },
   module: {
     loaders: [
@@ -40,7 +39,10 @@ module.exports = {
   },
   plugins: [
         new ExtractTextPlugin("styles.css"),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ]
 
 }
+
+module.exports = config;
